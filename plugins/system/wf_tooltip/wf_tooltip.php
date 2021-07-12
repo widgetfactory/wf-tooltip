ed4a4fabe9cc72627b63ddd0126c7733655d61e2
Ryan Demmer <ryandemmer@icloud.com>
2021-07-12 10:04:10 +0100

    Add selector option and update js path

--- plugins/system/wf_tooltip/wf_tooltip.php
+++ plugins/system/wf_tooltip/wf_tooltip.php
@@ -29,11 +29,15 @@ class PlgSystemWf_Tooltip extends JPlugin
         // Include jQuery
         JHtml::_('jquery.framework');
 
-        $selector   = $this->params->get('selector', '.jcetooltip');
-        $options    = array('position' => 'bottom-center');
-        $options    = json_encode($options);
+        $selector = $this->params->get('selector', '.jcetooltip');
+        $options = array(
+            'position' => 'bottom-center', 
+            'selector' => $selector
+        );
 
-        $document->addScript(JURI::base(true) . '/plugins/system/wf_tooltip/js/tooltip.js', array('version'));
+        $options = json_encode($options);
+
+        $document->addScript(JURI::base(true) . '/plugins/system/wf_tooltip/js/tooltip.min.js', array('version'));
         $document->addScriptDeclaration('jQuery(document).ready(function($){$("' . $selector . '").wf_tooltip(' . $options . ');});');
 
         $document->addStyleSheet(JURI::base(true) . '/plugins/system/wf_tooltip/css/tooltip.min.css', array('version'));
